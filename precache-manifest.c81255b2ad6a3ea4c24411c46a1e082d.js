b10588645345d9604c6d3369b710398f32b73c3b
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-02-20 16:05:24 +0300

    Updates

---
+++ precache-manifest.c81255b2ad6a3ea4c24411c46a1e082d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48319d85ec059303e1e",
+    "url": "/todobootstrap/static/css/main.39a2d6e4.chunk.css"
+  },
+  {
+    "revision": "b48319d85ec059303e1e",
+    "url": "/todobootstrap/static/js/main.b48319d8.chunk.js"
+  },
+  {
+    "revision": "6ee05bb36191ab9d7dd1",
+    "url": "/todobootstrap/static/js/runtime~main.6ee05bb3.js"
+  },
+  {
+    "revision": "f220c8adbaa1fb703f8a",
+    "url": "/todobootstrap/static/css/2.c1a64e71.chunk.css"
+  },
+  {
+    "revision": "f220c8adbaa1fb703f8a",
+    "url": "/todobootstrap/static/js/2.f220c8ad.chunk.js"
+  },
+  {
+    "revision": "174bb19546e55643253049444c4aefcc",
+    "url": "/todobootstrap/static/media/opera2.174bb195.jpg"
+  },
+  {
+    "revision": "58702527d6f187384ca06b8ce904c16d",
+    "url": "/todobootstrap/index.html"
+  }
+];
